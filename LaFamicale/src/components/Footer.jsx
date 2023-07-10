c281633520e12a32fda0a48a494d956eb1b828d2
JeremyPromsy <jeremypromsy@gmail.com>
2023-07-10 15:32:42 +0200

    Première vue

---
+++ LaFamicale/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/footer.css"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="copyright">© 2020 Kasa. All rights reserved</div>
+        </div>
+    )
+}
+
+export default Footer;
