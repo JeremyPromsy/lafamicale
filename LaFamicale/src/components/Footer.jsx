b6af3bfa06c65065b0911f5ec9d103ac1246ab32
JeremyPromsy <jeremypromsy@gmail.com>
2023-07-12 16:30:12 +0200

    Finalisation Footer

--- LaFamicale/src/components/Footer.jsx
+++ LaFamicale/src/components/Footer.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logoNav from "../assets/LOGO-famicale1-01.png";
 import "../styles/footer.css"
 
 function Footer() {
     return (
         <div className="footer">
-            <div className="logoFoot">
-            <Link to="/"><img className="logoFooter" src= {logoNav} alt ="logo LaFamicale" /> </Link>
-            </div>
+                <NavLink to="/">
+                    <img className="logoFooter" 
+                    src= {logoNav} 
+                    alt ="logo LaFamicale" /> 
+                </NavLink>
             <div className="nav-linksFooter">
-                <Link to="/mentionslégales">Mentions Légales
-            </Link>
+                <Link to="/mentionslégales">Mentions Légales</Link>
             </div>
         </div>
     )
