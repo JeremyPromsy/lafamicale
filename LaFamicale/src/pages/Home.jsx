a1c047135c10b308d7abcddd5148b3d14027f992
JeremyPromsy <jeremypromsy@gmail.com>
2023-07-12 15:12:35 +0200

    Avancement

--- LaFamicale/src/pages/Home.jsx
+++ LaFamicale/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/home.css"
+import Slideshow from "../components/Slideshow";
 
 function Home () {
     return (
         <div className="home">
-           
+           <Slideshow />
         </div>
     )
 }
