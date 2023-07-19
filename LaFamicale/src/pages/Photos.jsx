8503223545d98004916296c1d76b74f14fdd177e
JeremyPromsy <jeremypromsy@gmail.com>
2023-07-19 10:11:29 +0200

    MAJ

--- LaFamicale/src/pages/Photos.jsx
+++ LaFamicale/src/pages/Photos.jsx
@@ -13,15 +13,15 @@ import Img9 from "../Images/couloir-vert-LaFamicale.jpg"
 
 function galeriePhoto() {
     <div id="galeriePhoto" className="galeriePhoto"> 
-        <img className="photo1" src= {Img1} alt ="Salon La Famicale" /> 
-        <img className="photo2" src= {Img2} alt ="Salon La Famicale" />
-        <img className="photo3" src= {Img3} alt ="Cuisine La Famicale" />
-        <img className="photo4" src= {Img4} alt ="Chambre Parentale La Famicale" />
-        <img className="photo5" src= {Img5} alt ="Salle d'eau La Famicale" />
-        <img className="photo6" src= {Img6} alt ="Terrasse La Famicale" />
-        <img className="photo7" src= {Img7} alt ="Chambre amis La Famicale" />
-        <img className="photo8" src= {Img8} alt ="Chambre amis La Famicale" />
-        <img className="photo9" src= {Img9} alt ="Couloir La Famicale" />
+        <img className="photo photo1" src= {Img1} alt ="Salon La Famicale" /> 
+        <img className="photo photo2" src= {Img2} alt ="Salon La Famicale" />
+        <img className="photo photo3" src= {Img3} alt ="Cuisine La Famicale" />
+        <img className="photo photo4" src= {Img4} alt ="Chambre Parentale La Famicale" />
+        <img className="photo photo5" src= {Img5} alt ="Salle d'eau La Famicale" />
+        <img className="photo photo6" src= {Img6} alt ="Terrasse La Famicale" />
+        <img className="photo photo7" src= {Img7} alt ="Chambre amis La Famicale" />
+        <img className="photo photo8" src= {Img8} alt ="Chambre amis La Famicale" />
+        <img className="photo photo9" src= {Img9} alt ="Couloir La Famicale" />
     </div>
 }
 
