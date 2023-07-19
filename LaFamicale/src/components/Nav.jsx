8e4ab063b2ad2ac4ef13ac8e66e1e4c83ab2aa19
JeremyPromsy <jeremypromsy@gmail.com>
2023-07-19 10:38:09 +0200

    MAJ

---
+++ LaFamicale/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logoNav from "../assets/LOGO-famicale1-01.png";
+import "../styles/nav.css"
+
+function Navbar() {
+    return (
+        <div className="nav">
+            <div className="nav-links">
+                <NavLink to="/galeriephotos">Galerie Photos</NavLink>
+            </div>
+            <div className="logo">
+            <NavLink to="/"><img className="logo-img" src= {logoNav} alt ="logo LaFamicale" /> </NavLink>
+            </div>
+            <div className="nav-links">
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar ;
