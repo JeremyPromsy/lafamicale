5f1ba7260894a253868a799e0e71c03e8780984d
JeremyPromsy <jeremypromsy@gmail.com>
2023-11-15 15:36:00 +0100

    Avancement

--- my-app/src/components/Description.jsx
+++ my-app/src/components/Description.jsx
@@ -1,15 +1,45 @@
-import "../styles/description.css"
-import 'lightbox.js-react/dist/index.css'
+import "../styles/description.css";
+import 'lightbox.js-react/dist/index.css';
+import React, { useState } from 'react';
+import haut from "../assets/FlecheHaut.png";
+import bas from "../assets/FlècheBas.png";
 
+function Description({ title, content, open }) {
+    const [isOpen, setIsOpen] = useState(open);
 
-function Description(){
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const displayContent = () => {
+        if (Array.isArray(content)) {
+            return (
+                <ul>
+                    {content.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            );
+        }
+        return <p>{content}</p>;
+    };
+
     return (
-        <div>
-
-            
+        <div className={isOpen ? "collapseFermé" : "collapseOuvert"}>
+            <div className="container">
+                <h3>{title}</h3>
+                <img
+                    className={isOpen ? "FlècheHaute" : "FlècheBasse"} src={isOpen ? haut : bas} alt={isOpen ? "contenu caché" : "contenu affiché"}
+                    onClick={toggleOpen}
+                />
+            </div>
+            {isOpen && (
+                <div>
+                    {displayContent()}
+                </div>
+            )}
         </div>
-
-    )
+    );
 }
 
-export default Description ;
+export default Description;
